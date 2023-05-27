9c18a8c1be240ef4fac3da67303ac5ff2d66c4f3
Lucas-Mol <lucasmolpro@outlook.com>
2023-05-27 09:47:26 -0300

    feat(function_opts_enum): adding new Security Header Checker function

--- common/function_opts_enum.py
+++ common/function_opts_enum.py
@@ -1,13 +1,14 @@
-from services import http_banner_grabber_service, subdomain_scanner_service, tcp_port_scanner_service, web_scrapper_service
+from services import http_banner_grabber_service, subdomain_scanner_service, tcp_port_scanner_service, web_scrapper_service, security_header_checker_service
 from common import helper
 from enum import Enum
 
 class FunctionOptions(Enum):
-    HTTP_BANNER_GRABBER =   (1, '-hbg', http_banner_grabber_service)
-    TCP_PORT_SCANNER =      (2, '-tps', tcp_port_scanner_service)
-    SUBDOMAIN_SCANNER =     (3, '-ss',  subdomain_scanner_service)
-    WEB_SCRAPPER =          (4, '-ws',  web_scrapper_service)
-    HELPER =                (5, '-h', helper)
+    HTTP_BANNER_GRABBER =       (1, '-hbg', http_banner_grabber_service)
+    TCP_PORT_SCANNER =          (2, '-tps', tcp_port_scanner_service)
+    SUBDOMAIN_SCANNER =         (3, '-ss',  subdomain_scanner_service)
+    WEB_SCRAPPER =              (4, '-ws',  web_scrapper_service)
+    SECURITY_HEADER_CHECKER =   (5, '-shc', security_header_checker_service)
+    HELPER =                    (6, '-h', helper)
 
     def __init__(self, number, command, _class):
         self.number = number
